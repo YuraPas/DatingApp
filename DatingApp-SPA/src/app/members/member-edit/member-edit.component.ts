380f236cbd3fd77d9099f326e7057bb75636565b
Yuriy <yuriy.paslavsky@gmail.com>
2019-02-23 01:05:45 +0200

    Added Member edit page with a route guard to protect unsaved changes

---
+++ DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit, ViewChild, HostListener } from '@angular/core';
+import { User } from 'src/app/_models/user';
+import { ActivatedRoute } from '@angular/router';
+import { AlertifyService } from 'src/app/_services/alertify.service';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-member-edit',
+  templateUrl: './member-edit.component.html',
+  styleUrls: ['./member-edit.component.css']
+})
+export class MemberEditComponent implements OnInit {
+  // adding a view child decorator to access edit form and to reset it
+  @ViewChild('editForm') editForm: NgForm;
+  user: User;
+  // prevent user from losing data, when user closes browser or tab
+  @HostListener('window:beforeunload', ['$event'])
+  unloadNotification($event: any) {
+    if (this.editForm.dirty) {
+      $event.returnValue = true;
+    }
+  }
+
+  constructor(private route: ActivatedRoute, private alertify: AlertifyService) { }
+
+  ngOnInit() {
+    this.route.data.subscribe(data => {
+      this.user = data['user'];
+    });
+  }
+
+  updateUser() {
+    console.log(this.user);
+    this.alertify.success('Profile updated successfully');
+    // reseting dirty state to untouched so no alert will be shown and button will be dissabled
+    this.editForm.reset(this.user); // adding a paramether to reset form values to what they are, as they are saved
+  }
+
+}
